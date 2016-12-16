65bdb2f9e76c20cc3b94648ac94ae5c13ce09ce8
Kamal Arieff <kamalarieff@KAMAL-ARIEFF.local>
2016-12-17 03:11:19 +0800

    Print the html of the google page

--- automate_form.py
+++ automate_form.py
@@ -1,26 +1,12 @@
 import mechanize
-# import ssl
-# try:
-# 	_create_unverified_https_context = ssl._create_unverified_context
-# except AttributeError:
-# 	# Legacy Python that doesn't verify HTTPS certificates by default
-# 	pass
-# else:
-# 	# Handle target environment that doesn't support HTTPS verification
-# 	ssl._create_default_https_context = _create_unverified_https_context
 br = mechanize.Browser()
 # br.set_all_readonly(False)	# allow everything to be written to
 br.set_handle_robots(False)   # ignore robots
 br.set_handle_refresh(False)  # can sometimes hang without this
 br.addheaders =		 [('User-allowgent', 'Firefox')]
 
-br.open("https://www.google.com")
-# for form in br.forms():
-# 	print "Form name:", form.name
-# 	print form
+r = br.open('http://google.com')
+html = r.read()
 
-br.select_form('f')
-br.form['q'] = 'foo'
-response = br.submit()
-
-print response
+# Show the source
+print html # OR print br.response().read()
