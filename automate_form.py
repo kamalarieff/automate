e623c02b904657b794ba0069612787bcfa2d3063
Kamal Arieff <kamalarieff@KAMAL-ARIEFF.local>
2016-12-17 03:22:39 +0800

    Automate signing into controlpanel

--- automate_form.py
+++ automate_form.py
@@ -1,26 +1,32 @@
 import mechanize
+import ssl
+try:
+	_create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+	# Legacy Python that doesn't verify HTTPS certificates by default
+	pass
+else:
+	# Handle target environment that doesn't support HTTPS verification
+	ssl._create_default_https_context = _create_unverified_https_context
 br = mechanize.Browser()
 # br.set_all_readonly(False)	# allow everything to be written to
 br.set_handle_robots(False)   # ignore robots
 br.set_handle_refresh(False)  # can sometimes hang without this
 br.addheaders =		 [('User-allowgent', 'Firefox')]
 
-r = br.open('http://google.com')
+r = br.open('https://regress.mudah.my:31011/controlpanel')
 html = r.read()
 
 
 # Show the available forms
+# This is important
 for f in br.forms():
 	print f
 
-# Select the first (index zero) form
 br.select_form(nr=0)
 
-# Let's search
-br.form['q']='weekend codes'
+
+br.form['username']='kamal'
+br.form['cpasswd']='kamal'
 br.submit()
 print br.response().read()
-
-# Looking at some results in link format
-for l in br.links(url_regex='stockrt'):
-	print l
