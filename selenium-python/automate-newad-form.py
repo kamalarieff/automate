41ef06c0fcaeb9525146685ffdfd3e2774716f46
Kamal Arieff <kamalarieff@KAMAL-ARIEFF.local>
2016-12-17 17:53:41 +0800

    Print all the values of the dropdown and select all of them

--- selenium-python/automate-newad-form.py
+++ selenium-python/automate-newad-form.py
@@ -2,11 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome('/usr/local/bin/chromedriver')
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+driver.get("http://regress.mudah.my:31004/ai/form/0?ca=9_s")
+elem = driver.find_element_by_id("category_group")
+all_options = elem.find_elements_by_tag_name("option")
+for option in all_options:
+	print("Value is: %s" % option.get_attribute("value"))
+	option.click()
 driver.close()
