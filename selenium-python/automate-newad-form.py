f071cade72f22e2440ad813bee1f8d2cdac8c02e
Kamal Arieff <kamalarieff@KAMAL-ARIEFF.local>
2016-12-18 04:05:51 +0800

    Optimize code to fetch values from another file

--- selenium-python/automate-newad-form.py
+++ selenium-python/automate-newad-form.py
@@ -1,58 +1,23 @@
 from common import *
+from test import input_params
 
 driver = get_driver()
 
-select = Select(driver.find_element_by_id("category_group"))
-select.select_by_value("4180")
+for i in input_params["inputs"]:
+	try:
+		element = WebDriverWait(driver, 10).until(
+			EC.element_to_be_clickable((By.ID, str(i["element_id"])))
+		)
+	finally:
+		print 'Did not find element'
 
-try:
-	element = WebDriverWait(driver, 10).until(
-		EC.element_to_be_clickable((By.ID, "bag_type"))
-	)
-finally:
-	print 'Did not find element1'
-
-select = Select(driver.find_element_by_id("bag_type"))
-select.select_by_value("1")
-
-select = Select(driver.find_element_by_id("gender_type"))
-select.select_by_value("1")
-
-element = driver.find_element_by_id("subject")
-element.send_keys("test using selenium subject 6")
-
-element = driver.find_element_by_id("body")
-element.send_keys("test using selenium body")
-
-element = driver.find_element_by_id("price")
-element.send_keys("123")
-
-select = Select(driver.find_element_by_id("area"))
-select.select_by_value("628")
-
-element = driver.find_element_by_id("name")
-element.send_keys("kamal")
-
-element = driver.find_element_by_id("email")
-element.send_keys("kamal.arieff@gmail.com")
-
-element = driver.find_element_by_id("phone")
-element.send_keys("0129500092")
-
-element = driver.find_element_by_id("passwd")
-element.send_keys("123123")
-
-element = driver.find_element_by_id("passwd_ver")
-element.send_keys("123123")
-
-driver.find_element_by_id("c_publish").click()
-
-try:
-	element = WebDriverWait(driver, 10).until(
-		EC.element_to_be_clickable((By.ID, "photo-notification-btn-no"))
-	)
-finally:
-	print 'Did not find element'
-
-driver.find_element_by_id("photo-notification-btn-no").click()
-driver.quit()
+	if (str(i["type"]) == "dropdown"):
+		select = Select(driver.find_element_by_id(str(i["element_id"])))
+		select.select_by_value(str(i["value"]))
+	elif (str(i["type"]) == "text"):
+		element = driver.find_element_by_id(str(i["element_id"]))
+		element.send_keys(str(i["value"]))
+	elif (str(i["type"]) == "button"):
+		driver.find_element_by_id(str(i["element_id"])).click()
+		
+# driver.quit()
