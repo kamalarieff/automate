067b02e360f95b07cfe3113364b531284d76219a
Kamal Arieff <kamalarieff@KAMAL-ARIEFF.local>
2016-12-24 03:59:43 +0800

    Cannot use wait for input type file (images). For now it is just a hack, might be a bug there. Removed the photo-notification-btn-no because there is a photo uploaded now

--- selenium-python/automate-newad-form.py
+++ selenium-python/automate-newad-form.py
@@ -18,16 +18,17 @@ for k,v in data.items():
 	driver = get_driver()
 	for i in v:
 		try:
-			element = WebDriverWait(driver, 10).until(
-				EC.element_to_be_clickable((By.ID, str(i["element_id"])))
-			)
+			if (str(i["type"]) not in "image"):
+				element = WebDriverWait(driver, 60).until(
+					EC.element_to_be_clickable((By.ID, str(i["element_id"])))
+				)
 		finally:
 			print 'Did not find element'
 
 		if (str(i["type"]) == "dropdown"):
 			select = Select(driver.find_element_by_id(str(i["element_id"])))
 			select.select_by_value(str(i["value"]))
-		elif (str(i["type"]) == "text"):
+		elif (str(i["type"]) in ["text", "image"]):
 			element = driver.find_element_by_id(str(i["element_id"]))
 			element.send_keys(str(i["value"]))
 		elif (str(i["type"]) == "button" or (str(i["type"]) == "checkbox" and str(i["value"]) == "1")):
