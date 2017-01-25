1a3cc47371ba7271bc3671138d598cabe40a6995
Kamal Arieff <kamalarieff@github.com>
2017-01-25 10:09:19 +0800

    Added the config to point to insert ad in muddah

--- selenium-python/config.py
+++ selenium-python/config.py
@@ -1,3 +1,4 @@
 REGRESS_PORT = "310"
 NEWAD_URL = "http://regress.mudah.my:{}04/ai/form/0?ca=9_s".format(REGRESS_PORT)
+# NEWAD_URL = "http://www2.muddah.com/ai/form/0"
 HOMEPAGE_URL = "http://regress.mudah.my:{}08/index.htm".format(REGRESS_PORT)
