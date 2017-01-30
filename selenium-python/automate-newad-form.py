8ae2d18548d6b954212029fca816b2159d5c0b0f
Kamal Arieff <kamalarieff@github.com>
2017-01-30 23:00:04 +0800

    Use an OrderedDict to get the config from json

--- selenium-python/automate-newad-form.py
+++ selenium-python/automate-newad-form.py
@@ -1,4 +1,5 @@
 from common import *
+from collections import OrderedDict
 
 def print_usage():
 	print 'usage: python {} <inputfile>.json'.format(__file__)
@@ -16,7 +17,7 @@ if not os.path.isfile(filename):
 
 with open(filename) as data_file:	
 	try:
-		data = json.load(data_file)
+		data = json.load(data_file, object_pairs_hook=OrderedDict)
 	except ValueError:
 		print 'ERROR: Not a JSON file'
 		print_usage()
@@ -24,21 +25,23 @@ with open(filename) as data_file:
 
 for k,v in data.items():
 	print k 
-	driver = get_driver()
-	for i in v:
-		try:
-			if (str(i["type"]) not in "image"):
-				element = WebDriverWait(driver, 60).until(
-					EC.element_to_be_clickable((By.ID, str(i["element_id"])))
-				)
-		finally:
-			print 'Did not find element'
+	if k in "config":
+		driver = get_driver()
+	else:
+		for i in v:
+			try:
+				if (str(i["type"]) not in "image"):
+					element = WebDriverWait(driver, 60).until(
+						EC.element_to_be_clickable((By.ID, str(i["element_id"])))
+					)
+			finally:
+				print 'Did not find element'
 
-		if (str(i["type"]) == "dropdown"):
-			select = Select(driver.find_element_by_id(str(i["element_id"])))
-			select.select_by_value(str(i["value"]))
-		elif (str(i["type"]) in ["text", "image"]):
-			element = driver.find_element_by_id(str(i["element_id"]))
-			element.send_keys(str(i["value"]))
-		elif (str(i["type"]) == "button" or (str(i["type"]) == "checkbox" and str(i["value"]) == "1")):
-			driver.find_element_by_id(str(i["element_id"])).click()
+			if (str(i["type"]) == "dropdown"):
+				select = Select(driver.find_element_by_id(str(i["element_id"])))
+				select.select_by_value(str(i["value"]))
+			elif (str(i["type"]) in ["text", "image"]):
+				element = driver.find_element_by_id(str(i["element_id"]))
+				element.send_keys(str(i["value"]))
+			elif (str(i["type"]) == "button" or (str(i["type"]) == "checkbox" and str(i["value"]) == "1")):
+				driver.find_element_by_id(str(i["element_id"])).click()
