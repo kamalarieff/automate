76e13e0d187ccc6182b465e478b3521069a14661
Kamal Arieff <kamalarieff@github.com>
2017-03-29 16:13:43 +0800

    Choose deadline by using xpath

--- selenium-python/automate-newad-form.py
+++ selenium-python/automate-newad-form.py
@@ -69,10 +69,23 @@ for k,v in data.items():
 					element.send_keys(str(i["value"]))
 				elif (str(i["type"]) == "button" or str(i["type"]) == "checkbox"):
 					if 'multiple' in i:
-						list = driver.find_element_by_name(str(i["element_name"]))
+						list = driver.find_elements_by_name(str(i["element_name"]))
 						list[int(i["multiple"])].click()
 					else:
 						driver.find_element_by_name(str(i["element_name"])).click()
+			elif 'element_xpath' in i:
+				if (str(i["type"]) == "dropdown"):
+					select = Select(driver.find_element_by_xpath(str(i["element_xpath"])))
+					select.select_by_value(str(i["value"]))
+				elif (str(i["type"]) in ["text", "image"]):
+					element = driver.find_element_by_xpath(str(i["element_xpath"]))
+					element.send_keys(str(i["value"]))
+				elif (str(i["type"]) == "button" or str(i["type"]) == "checkbox"):
+					if 'multiple' in i:
+						list = driver.find_elements_by_xpath(str(i["element_xpath"]))
+						list[int(i["multiple"])].click()
+					else:
+						driver.find_element_by_xpath(str(i["element_xpath"])).click()
 			elif 'element_class' in i:
 				if (str(i["type"]) == "dropdown"):
 					select = Select(driver.find_element_by_class_name(str(i["element_class"])))
