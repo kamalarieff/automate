8c00b185c27ed20efc75d09a6066d745ff3ea417
Kamal Arieff <kamalarieff@github.com>
2017-03-31 00:43:53 +0800

    Use reflection for dynamic functions

--- selenium-python/automate-newad-form.py
+++ selenium-python/automate-newad-form.py
@@ -30,79 +30,24 @@ for k,v in data.items():
 	else:
 		for i in v:
 			if 'element_id' in i:
-				try:
-					if (str(i["type"]) not in "image"):
-						element = WebDriverWait(driver, 60).until(
-							EC.element_to_be_clickable((By.ID, str(i["element_id"])))
-						)
-				finally:
-					print 'Did not find element'
-
-				if (str(i["type"]) == "dropdown"):
-					select = Select(driver.find_element_by_id(str(i["element_id"])))
-					select.select_by_value(str(i["value"]))
-				elif (str(i["type"]) in ["text", "image"]):
-					element = driver.find_element_by_id(str(i["element_id"]))
+				temp = 'element_id'.split("_")
+				if (str(i["type"]) in ["text", "image"]):
+					temp1 = 'find_element_by_'+ temp[1]
+					element = getattr(driver, temp1)(str(i["element_id"]))
 					if 'clear' in i:
 						element.clear()
 					element.send_keys(str(i["value"]))
-				elif (str(i["type"]) == "button" or str(i["type"]) == "checkbox"):
-					if 'multiple' in i:
-						list = driver.find_elements_by_id(str(i["element_id"]))
-						list[int(i["multiple"])].click()
-					else:
-						driver.find_element_by_id(str(i["element_id"])).click()
-			elif 'element_name' in i:
+			if 'element_name' in i:
+				element = element_wait = str(i["element_name"])
 				try:
 					if (str(i["type"]) not in "image"):
 						element = WebDriverWait(driver, 60).until(
-							EC.element_to_be_clickable((By.NAME, str(i["element_name"])))
+							EC.element_to_be_clickable((By.NAME, element_wait))
 						)
 				finally:
 					print 'Did not find element'
-
-				if (str(i["type"]) == "dropdown"):
-					select = Select(driver.find_element_by_name(str(i["element_name"])))
-					select.select_by_value(str(i["value"]))
-				elif (str(i["type"]) in ["text", "image"]):
-					element = driver.find_element_by_name(str(i["element_name"]))
+				temp = 'element_name'.split("_")
+				if (str(i["type"]) in ["text", "image"]):
+					temp1 = 'find_element_by_'+ temp[1]
+					element = getattr(driver, temp1)(element)
 					element.send_keys(str(i["value"]))
-				elif (str(i["type"]) == "button" or str(i["type"]) == "checkbox"):
-					if 'multiple' in i:
-						list = driver.find_elements_by_name(str(i["element_name"]))
-						list[int(i["multiple"])].click()
-					else:
-						driver.find_element_by_name(str(i["element_name"])).click()
-			elif 'element_xpath' in i:
-				if (str(i["type"]) == "dropdown"):
-					select = Select(driver.find_element_by_xpath(str(i["element_xpath"])))
-					select.select_by_value(str(i["value"]))
-				elif (str(i["type"]) in ["text", "image"]):
-					element = driver.find_element_by_xpath(str(i["element_xpath"]))
-					element.send_keys(str(i["value"]))
-				elif (str(i["type"]) == "button" or str(i["type"]) == "checkbox"):
-					if 'multiple' in i:
-						list = driver.find_elements_by_xpath(str(i["element_xpath"]))
-						list[int(i["multiple"])].click()
-					else:
-						driver.find_element_by_xpath(str(i["element_xpath"])).click()
-			elif 'element_class' in i:
-				if (str(i["type"]) == "dropdown"):
-					select = Select(driver.find_element_by_class_name(str(i["element_class"])))
-					select.select_by_value(str(i["value"]))
-				elif (str(i["type"]) in ["text", "image"]):
-					element = driver.find_element_by_class_name(str(i["element_class"]))
-					element.send_keys(str(i["value"]))
-				elif (str(i["type"]) == "button" or str(i["type"]) == "checkbox"):
-					if 'multiple' in i:
-						list = driver.find_element_by_class_name(str(i["element_class"]))
-						list[int(i["multiple"])].click()
-					else:
-						driver.find_element_by_class_name(str(i["element_class"])).click()
-			elif 'element_link' in i:
-				if (str(i["type"]) == "link"):
-					if 'multiple' in i:
-						list = driver.find_elements_by_link_text(str(i["element_link"]))
-						list[int(i["multiple"])].click()
-					else:
-						driver.find_element_by_link_text(str(i["element_link"])).click()
