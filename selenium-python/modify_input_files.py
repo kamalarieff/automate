b23b732152aabe6b89e1801efd990104bfe016be
Kamal Arieff <kamalarieff@github.com>
2017-04-01 08:39:57 +0800

    Write back to the same file

--- selenium-python/modify_input_files.py
+++ selenium-python/modify_input_files.py
@@ -1,4 +1,9 @@
 from __future__ import print_function
+import sys, os
+
+def print_usage():
+	print ('usage: python {} <inputfile>.json'.format(__file__))
+	sys.exit(2)
 
 def add_json_entry(w, line, element):
 	num_tab = len(line) - len(line.lstrip('\t'))
@@ -9,11 +14,20 @@ def add_json_entry(w, line, element):
 	w.write(temp)
 	return line.replace(element,"attr")
 
-w = open('test.json', 'w')
-with open('staging/ai-cars-images.json', 'r') as f:
+try:
+	filename = str(sys.argv[1])
+except IndexError:
+	print ('ERROR: No input file')
+	print_usage()
+
+
+w = open('temp.json', 'w')
+with open(filename, 'r') as f:
 	for line in f:
 		if 'element_id' in line:
 			line = add_json_entry(w,line, element = 'element_id')
 		elif 'element_name' in line:
 			line = add_json_entry(w,line, element = 'element_name')
 		w.write(line)
+	
+os.rename('temp.json', filename)
