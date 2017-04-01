0b1fe45e209d082b51c57aad40494f72f9cace59
Kamal Arieff <kamalarieff@github.com>
2017-04-01 08:17:25 +0800

    Added support for element_name

--- selenium-python/modify_input_files.py
+++ selenium-python/modify_input_files.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 
 w = open('test.json', 'w')
-with open('staging/ai-cars-images-json', 'r') as f:
+with open('staging/ai-cars-images.json', 'r') as f:
 	for line in f:
 		if 'element_id' in line:
 			num_tab = len(line) - len(line.lstrip('\t'))
@@ -9,4 +9,10 @@ with open('staging/ai-cars-images-json', 'r') as f:
 			temp = tab+"\"element\":\"ID\",\n"
 			w.write(temp)
 			line = line.replace("element_id","attr")
+		elif 'element_name' in line:
+			num_tab = len(line) - len(line.lstrip('\t'))
+			tab = '\t'*num_tab
+			temp = tab+"\"element\":\"NAME\",\n"
+			w.write(temp)
+			line = line.replace("element_name","attr")
 		w.write(line)
