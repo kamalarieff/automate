ff345eee720f4d0fc8275a2dc47a8b72cd1fa3a9
Kamal Arieff <kamalarieff@github.com>
2017-04-01 08:15:17 +0800

    Write to file

--- selenium-python/modify_input_files.py
+++ selenium-python/modify_input_files.py
@@ -1,12 +1,12 @@
 from __future__ import print_function
 
-f = open('temp2.json', 'r')
-with open('temp2.json', 'r') as f:
+w = open('test.json', 'w')
+with open('staging/ai-cars-images-json', 'r') as f:
 	for line in f:
 		if 'element_id' in line:
 			num_tab = len(line) - len(line.lstrip('\t'))
 			tab = '\t'*num_tab
-			temp = tab+"\"element\":\"ID\","
-			print (temp)
+			temp = tab+"\"element\":\"ID\",\n"
+			w.write(temp)
 			line = line.replace("element_id","attr")
-		print (line, end="")
+		w.write(line)
