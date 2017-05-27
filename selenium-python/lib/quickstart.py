2f603e33562c66a19f46477d3ebe0465a80b923f
Kamal Arieff <kamalarieff@github.com>
2017-05-28 05:24:53 +0800

    [I3] Reverted back to not using classes

--- selenium-python/lib/quickstart.py
+++ selenium-python/lib/quickstart.py
@@ -14,60 +14,61 @@ CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Google Sheets API Python Quickstart'
 
 
-class Sheets:
-	def get_credentials(self):
-		"""Gets valid user credentials from storage.
+def get_credentials():
+	"""Gets valid user credentials from storage.
 
-		If nothing has been stored, or if the stored credentials are invalid,
-		the OAuth2 flow is completed to obtain the new credentials.
+	If nothing has been stored, or if the stored credentials are invalid,
+	the OAuth2 flow is completed to obtain the new credentials.
 
-		Returns:
-			Credentials, the obtained credential.
-		"""
-		home_dir = os.path.expanduser('~')
-		credential_dir = os.path.join(home_dir, '.credentials')
-		if not os.path.exists(credential_dir):
-			os.makedirs(credential_dir)
-		credential_path = os.path.join(credential_dir,
-									   'sheets.googleapis.com-python-quickstart.json')
+	Returns:
+		Credentials, the obtained credential.
+	"""
+	home_dir = os.path.expanduser('~')
+	credential_dir = os.path.join(home_dir, '.credentials')
+	if not os.path.exists(credential_dir):
+		os.makedirs(credential_dir)
+	credential_path = os.path.join(credential_dir,
+								   'sheets.googleapis.com-python-quickstart.json')
 
-		store = Storage(credential_path)
-		credentials = store.get()
-		if not credentials or credentials.invalid:
-			flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-			flow.user_agent = APPLICATION_NAME
-			if flags:
-				credentials = tools.run_flow(flow, store, flags)
-			else: # Needed only for compatibility with Python 2.6
-				credentials = tools.run(flow, store)
-			print('Storing credentials to ' + credential_path)
-		return credentials
+	store = Storage(credential_path)
+	credentials = store.get()
+	if not credentials or credentials.invalid:
+		flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+		flow.user_agent = APPLICATION_NAME
+		if flags:
+			credentials = tools.run_flow(flow, store, flags)
+		else: # Needed only for compatibility with Python 2.6
+			credentials = tools.run(flow, store)
+		print('Storing credentials to ' + credential_path)
+	return credentials
 
-	def test(self):
-		"""Shows basic usage of the Sheets API.
+def test():
+	"""Shows basic usage of the Sheets API.
 
-		Creates a Sheets API service object and prints the names and majors of
-		students in a sample spreadsheet:
-		https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-		"""
-		credentials = self.get_credentials()
-		http = credentials.authorize(httplib2.Http())
-		discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
-						'version=v4')
-		service = discovery.build('sheets', 'v4', http=http,
-								  discoveryServiceUrl=discoveryUrl)
+	Creates a Sheets API service object and prints the names and majors of
+	students in a sample spreadsheet:
+	https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+	"""
+	credentials = get_credentials()
+	http = credentials.authorize(httplib2.Http())
+	discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+					'version=v4')
+	service = discovery.build('sheets', 'v4', http=http,
+							  discoveryServiceUrl=discoveryUrl)
 
-		spreadsheetId = '1QvIKJiT5TbpZtMr7mNyG2bPcN6tHjVBf3v4f4nEmc4k'
-		rangeName = 'Sheet1!A6:C6'
-		result = service.spreadsheets().values().get(
-			spreadsheetId=spreadsheetId, range=rangeName).execute()
-		values = result.get('values', [])
+	spreadsheetId = '1QvIKJiT5TbpZtMr7mNyG2bPcN6tHjVBf3v4f4nEmc4k'
+	rangeName = 'Sheet1!A6:D6'
+	result = service.spreadsheets().values().get(
+		spreadsheetId=spreadsheetId, range=rangeName).execute()
+	values = result.get('values', [])
 
-		if not values:
-			print('No data found.')
-		else:
-			print('Name, Major:')
-			for row in values:
-				# Print columns A and E, which correspond to indices 0 and 4.
-				# print('%s', '%s' % (row[0], row[1]))
-				print(row[2])
+	if not values:
+		print('No data found.')
+	else:
+		print('Name, Major:')
+		for row in values:
+			# Print columns A and E, which correspond to indices 0 and 4.
+			# print('%s', '%s' % (row[0], row[1]))
+			print(row[2])
+
+	return values
